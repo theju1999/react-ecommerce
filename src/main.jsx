7676dd84717d39d9c6d5d05d27ecf38828fbbac4
theju1999 <91662739+theju1999@users.noreply.github.com>
2024-04-20 23:01:45 +0530

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap's CSS file
 import App from './App.jsx'
 import './index.css'
 
